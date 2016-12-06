50c44fabaf55e37361fa8c7dbce1f74903f7d5a3
Khanh Duong <kduong@mymail.mines.edu>
2016-12-06 15:40:20 -0700

    Finished queries 1-4

--- Pokemon.sql
+++ Pokemon.sql
@@ -1,3 +1,28 @@
+/* %s is where the string insert will be */
+/* ie in Java => "... WHERE id =" + %s */ 
+
+
+
+/*	Query: 1	*/
+SELECT identifier 
+FROM pokemon_species
+WHERE id = %s;
+
+/*	Query: 2	*/
+SELECT s.identifier, ps.base_stat
+FROM pokemon_stats AS ps, stats AS s
+WHERE ps.pokemon_id = %s AND s.id = ps.stat_id;
+
+/*	Query: 3	*/
+SELECT t.identifier
+FROM pokemon_types AS pt, types AS t
+WHERE pt.pokemon_id = %s AND t.id = pt.type_id;
+
+/*	Query:4 	*/
+SELECT g.identifier
+FROM generations AS g, pokemon_species AS ps
+WHERE ps.id = %s AND g.id = ps.generation_id;
+
 /*	Query: 9	*/
 select identifier
 from pokemon_species
