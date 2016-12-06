a1ed41e84e30751993c57254f1c3fd079eca716e
Khanh Duong <kduong@mymail.mines.edu>
2016-12-06 15:45:57 -0700

    Tidied up some things in the .sql

--- Pokemon.sql
+++ Pokemon.sql
@@ -18,32 +18,40 @@ SELECT t.identifier
 FROM pokemon_types AS pt, types AS t
 WHERE pt.pokemon_id = %s AND t.id = pt.type_id;
 
-/*	Query:4 	*/
+/*	Query: 4 	*/
 SELECT g.identifier
 FROM generations AS g, pokemon_species AS ps
 WHERE ps.id = %s AND g.id = ps.generation_id;
 
+/*	Query: 5	*/
+
+/*	Query: 6	*/
+
+/*	Query: 7	*/
+
+/*	Query: 8	*/
+
 /*	Query: 9	*/
-select identifier
-from pokemon_species
-where generation_id = %s;
+SELECT identifier
+FROM pokemon_species
+WHERE generation_id = %s;
 
 /*	Query: 10	*/
-select distinct ps.pokemon_id 
-from pokemon_stats as ps, stats as s
-where s.identifier = %s AND ps.base_stat >= %s 
-order by ps.pokemon_id;
+SELECT distinct ps.pokemon_id 
+FROM pokemon_stats AS ps, stats AS s
+WHERE s.identifier = %s AND ps.base_stat >= %s 
+ORDER by ps.pokemon_id;
 
 /*	Query: 11	*/
-select p.pokemon_id 
-from (select pokemon_id, sum(base_stat) 
-	from pokemon_stats 
-	group by pokemon_id 
-	order by pokemon_id asc) as p 
-where sum >= %s;
+SELECT p.pokemon_id 
+FROM (select pokemon_id, sum(base_stat) 
+	FROM pokemon_stats 
+	GROUP BY pokemon_id 
+	ORDER BY pokemon_id asc) AS p 
+WHERE sum >= %s;
 
 /*	Query: 12	*/
-select pt.pokemon_id 
-from pokemon_types as pt, types as t 
-where t.identifier = %s AND t.id = pt.type_id;
+SELECT pt.pokemon_id 
+FROM pokemon_types AS pt, types AS t 
+WHERE t.identifier = %s AND t.id = pt.type_id;
 
