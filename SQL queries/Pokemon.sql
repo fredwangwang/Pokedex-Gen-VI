a5cd4cde33384d7f70747ae55d568461103490cf
Michael Villafuerte <mvillafu@mymail.mines.edu>
2016-12-06 22:39:06 -0700

    Changed the comments to be what the query does.

--- SQL queries/Pokemon.sql
+++ SQL queries/Pokemon.sql
@@ -4,32 +4,32 @@
 /* Formalized query: output [id, identifier, type] for original queries which return id
 /* Note: For those have two types, concate them into single string. e.g. 'Normal, Water'
 
-/*	Query: 1	*/
+/*	1. Given national pokedex id, find the name of the pokemon	*/
 SELECT identifier 
 FROM pokemon_species
 WHERE id = %s;
 
-/*	Query: 2	*/
+/*	2. Given national pokedex id, find the stats of the pokemon	*/
 SELECT s.identifier, ps.base_stat
 FROM pokemon_stats AS ps, stats AS s
 WHERE ps.pokemon_id = %s AND s.id = ps.stat_id;
 
-/*	Query: 3	*/
+/*	3. Given national pokedex id, find the type of pokemon	*/
 SELECT t.identifier
 FROM pokemon_types AS pt, types AS t
 WHERE pt.pokemon_id = %s AND t.id = pt.type_id;
 
-/*	Query: 4 	*/
+/*	4. Given national pokedex id, find the generation of that pokemon	*/
 SELECT g.identifier
 FROM generations AS g, pokemon_species AS ps
 WHERE ps.id = %s AND g.id = ps.generation_id;
 
-/*	Query: 5	*/
+/*	5. Given national pokedex id, find what is the id of the pokemon it evolves from	*/
 SELECT evolves_from_species_id
 FROM pokemon_species
 WHERE id = %s;
 
-/*	Query: 6	*/
+/*	6. Given national pokedex id, find the id of all ancestor and descendant of that pokemon	*/
 SELECT id
 FROM  pokemon_species
 WHERE id<>%s
@@ -37,28 +37,28 @@ AND evolution_chain_id = (SELECT evolution_chain_id
 	FROM pokemon_species
 	WHERE id=%s);
 
-/*	Query: 7	*/
+/*	7. Given generation id, find the region	*/
 SELECT p.generation_id, r.identifier
 FROM pokemon_species as p, regions as r
 WHERE p.id=%s AND p.generation_id=r.id;
 
-/*	Query: 8	*/
+/*	8. Given ancestor id and pokemon id, find all evolve conditions	*/
 SELECT s.identifier as "To-get", t.identifier as "Method"
 FROM evolution_triggers as t, pokemon_evolution as e, pokemon_species as s
 WHERE t.id=e.evolution_trigger_id AND e.evolved_species_id=%s AND s.id=%s;
 
-/*	Query: 9	*/
+/*	9. Given generation id, find all pokemon of that generation	*/
 SELECT identifier
 FROM pokemon_species
 WHERE generation_id = %s;
 
-/*	Query: 10	*/
+/*	10. Given condition stats, find all ids of qualified pokemon	*/
 SELECT distinct ps.pokemon_id 
 FROM pokemon_stats AS ps, stats AS s
 WHERE s.identifier = %s AND ps.base_stat >= %s 
 ORDER by ps.pokemon_id;
 
-/*	Query: 11	*/
+/*	11. Given stats sum, find all ids of qualified pokemon	*/
 SELECT p.pokemon_id 
 FROM (select pokemon_id, sum(base_stat) 
 	FROM pokemon_stats 
@@ -66,7 +66,7 @@ FROM (select pokemon_id, sum(base_stat)
 	ORDER BY pokemon_id asc) AS p 
 WHERE sum >= %s;
 
-/*	Query: 12	*/
+/*	12. Given certain type, find ids of all pokemon of that type	*/
 SELECT pt.pokemon_id 
 FROM pokemon_types AS pt, types AS t 
 WHERE t.identifier = %s AND t.id = pt.type_id;
